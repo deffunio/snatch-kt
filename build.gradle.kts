14b60b4111729b84fd2ae12394ede202db66bd7a
Alexandr <99070761+AlexVoin04@users.noreply.github.com>
2023-10-31 15:25:02 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,14 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/AlexVoin04/snatch-kt")
+        credentials {
+            username = System.getenv("GITHUB_ACTOR")
+            password = System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 dependencies {
