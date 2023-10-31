124783011136b234df0c909ae2e6bb2341931b4a
Alexandr <99070761+AlexVoin04@users.noreply.github.com>
2023-10-31 17:13:57 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.test {
