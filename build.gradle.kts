6aed79a421310857a3ad939d350f7a19bde5c14b
artem <artem@deffun.io>
2023-10-31 13:16:57 +0700

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "io.deffun"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
