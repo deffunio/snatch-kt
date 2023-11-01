57ef306dd4e7a9cd3997413e0d3f2b8e6dde6989
Alex <alexiv.04@mail.ru>
2023-11-01 12:43:37 +0700

    add few lines

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.0"
     id("maven-publish")
+//    id("java-library")
 }
 
 group = "io.deffun"
 version = "0.1"
 
 repositories {
-    maven {
-        name = "GitHubPackages"
-        url = uri("https://maven.pkg.github.com/AlexVoin04/snatch-kt")
-        credentials {
-            username = System.getenv("GITHUB_ACTOR")
-            password = System.getenv("GITHUB_TOKEN")
-        }
-    }
     mavenCentral()
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/AlexVoin04/snatch-kt")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
 
 dependencies {
     testImplementation(kotlin("test"))
@@ -28,8 +40,4 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(17)
 }
